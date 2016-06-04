c1214d0e62fa951b7644782d5cdfa2f69b9b1d0a
Preston Pham <ppham@us.ibm.com>
2016-06-04 16:15:06 -0500

    permits api

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var questions = require('../lib/questions');
+var permits = require('../lib/permits');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// API
 router.get('/questions/:id', function(req, res, next) {
   if (questions[req.params.id]) {
     return res.json(questions[req.params.id]);
   }
   res.status(404).send('Question not found');
+});
+
+router.get('/permits/:id', function(req, res, next) {
+  if (permits[req.params.id]) {
+    return res.json(permits[req.params.id]);
+  }
+  res.status(404).send('Permit not found');
 });
 
 module.exports = router;
