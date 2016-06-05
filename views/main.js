3f7236c8f2f449ee405a6355baa9bb040bc85339
Preston Pham <ppham@us.ibm.com>
2016-06-04 19:56:49 -0500

    jquery flow

--- views/main.js
+++ views/main.js
@@ -1,5 +1,7 @@
 var $question = $('#question');
 var $answers = $('#answers');
+var $back = $('#back');
+
 var current;
 var checklist = [];
 var actions = [];
@@ -7,29 +9,52 @@ var actions = [];
 function renderAnswer(text) {
   var $answer = $('<button>' + text + '</button>');
   $answer.click(function() {
-    var id = current.answers[text];
+    var id = current.answers[text].next;
     current.selected = text;
     actions.push(current);
+
+    if (current.answers[text].checklist) {
+      checklist.push(current.answers[text].checklist);
+    }
+
     getQuestion(id);
   });
   return $answer;
 }
 
 function render(data) {
+  // Set current data
   current = data;
+
+  // TODO Check if there is a permid id to lookup
+
+  // Question
   $question.html(current.text);
+
+  // Answers
   var answers = Object.keys(current.answers).map(renderAnswer);
   if (answers.length)
     $answers.html(answers);
   else
     $answers.html('');
 
-  // if (actions.length) {
-  //   $backBtn = $('<button>Back</button>');
-  //   $back.html($backBtn);
-  // } else {
-  //   $back.html('');
-  // }
+  // Back Button
+  if (actions.length) {
+    var $backBtn = $('<button>Back</button>');
+    $backBtn.click(function() {
+      var previous = actions.pop();
+
+      var checklistIdx = checklist.indexOf(previous.checklist);
+      if (checklistIdx >= 0) {
+        checklist.splice(checklistIdx, 1);
+      }
+
+      render(actions.pop());
+    });
+    $back.html($backBtn);
+  } else {
+    $back.html('');
+  }
 }
 
 function renderFailure() {
@@ -51,4 +76,5 @@ function diagnose() {
   console.log('Actions', actions);
   console.log('Checklist', checklist);
 }
+
 getQuestion(1);
