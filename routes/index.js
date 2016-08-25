b76c0256f14a410f72db2ebff42f00d7e68dbe2d
Preston Pham <ppham@us.ibm.com>
2016-08-25 16:52:35 -0500

    add zoning api

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var questions = require('../lib/questions');
 var permits = require('../lib/permits');
+var geo = require('../lib/geo');
 
 router.get('/questions/:id', function(req, res, next) {
   if (questions[req.params.id]) {
@@ -26,6 +27,24 @@ router.get('/prereqs/:id', function(req, res, next) {
     return res.json(prereqs[req.params.id]);
   }
   res.status(404).send('Permit not found');
+});
+
+router.get('/api/zones', function(req, res, next) {
+  if (!req.query.address) {
+    return res.status(500).send('address query is required');
+  }
+
+  geo.getAddressCandidates(req.query.address, function(err, response) {
+    if (err) return next(err);
+    geo.inHistoricalDistrict(response.candidates[0].location, function(err, isInHistoricalDistrict) {
+      if (err) return next(err);
+      res.json({
+        address: req.query.address,
+        location: response.candidates[0].location,
+        historic_district: isInHistoricalDistrict
+      });
+    });
+  });
 });
 
 
